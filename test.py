1665fb6ed1bcac75b7133edf4d5eea5674fab870
root <root@AEIM>
2024-03-30 07:16:32 -0400

    Added a test file.
    Writing algorithm for controlling the locations of the injection locations instead randomly choosing points.

--- test.py
+++ test.py
@@ -2,27 +2,48 @@ import csv
 
 # Read in the coordinates from the text file
 with open('s1 blade.txt', 'r') as f:
-    coordinates = f.read().strip().splitlines()
+    raw_data = f.read().strip().splitlines()
 
-# Coordinates
-x_coords = []
-y_coords = []
-z_coords = []
+# Individual coordinates
+x_values = []
+y_values = []
+z_values = []
 
 # Separate and Scale the Coordinates
-for line in coordinates:
+for line in raw_data:
     # Separate the coordinates into x, y, and z coordinates
     coord_set = line.split(',')
 
     # Add each coordinate to its corresponding list and apply its scientific notation
-    x_coords.append(float(coord_set[0][:-4]) / 10**(int(coord_set[0][-1:])))
-    y_coords.append(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:])))
-    z_coords.append(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:])))
+    x_value = str(float(coord_set[0][:-4]) / 10**(int(coord_set[0][-1:])))
+    y_value = str(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:])))
+    z_value = str(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:])))
+    
+    x_values.append(x_value)
+    y_values.append(x_value)
+    z_values.append(x_value)
+    
+# Coordinates as points
+unsorted_coordinates = tuple(zip(x_values, y_values, z_values))
 
-# Match the x, y, and z coordinates with each other
-push_coordinates = tuple(zip(x_coords, y_coords, z_coords))
 
-filename = 'injection regions.csv'
+tip = max(x_value)
+root = min(x_value)
+sorted_x = x_values.sort()
+
+# Identify the coordinates around this general location 
+approximate_x = '0.598'
+
+# Filter the coordinates by the general location
+push_coordinates = [coordinate for coordinate in unsorted_coordinates if coordinate[0].find(approximate_x) != -1]
+
+'''
+Write the injection locations to a file
+
+NOTE: Code works. Don't change it.
+'''
+
+filename = 'test regions.csv'
 
 # Erase the contents of the file
 open(filename, 'w').close()
