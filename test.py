5fbd9dbcceb0d643e7e2f20145d9d96dfc91857a
root <root@AEIM>
2024-03-30 09:17:58 -0400

    defined coordinates based on a common x-value
    Filtered the coordinates using a user-defined x-value. The result resembles the airfoil of the blade (but is not due to the angle between the blade and the center of the hub.
    
    Moved the code that writes the coordinates to a file into a function. Also turned the code tht finds the coordinatesinto a function as well.

--- test.py
+++ test.py
@@ -1,5 +1,44 @@
 import csv
 
+
+def write_coords(sorted_coordinates, filename):
+    '''
+    Writes the injection locations to a CSV file
+    '''
+
+    # Erase the contents of the file
+    open(filename, 'w').close()
+
+    # Open the file to use csv module
+    with open(filename, 'w', newline='\n') as csvfile:
+        coord_writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+
+        # File Format Requirements
+        csvfile.write('[Name]\nS1 Blade\n\n')
+        csvfile.write('[Spatial Fields]\n')
+        coord_writer.writerow(['X', 'Y', 'Z'])
+        csvfile.write('\n[Data]\n')
+        coord_writer.writerow(['X[m]', 'Y[m]', 'Z[m]'])
+        csvfile.write('\n')
+
+        # Write each set of coordinates as a row in the file
+        for point in sorted_coordinates:
+            coord_writer.writerow([point[0], point[1], point[2]])
+
+
+def cross_section(x_location, precision):
+    # Identify the general x-location for the data set
+    approximate_x = str(sorted_x[int(len(sorted_x) * x_location)])[0:precision]
+
+    # Coordinates as points
+    coordinates = list(zip(x_values, y_values, z_values))
+
+    # Filter the coordinates by the common x-value
+    sorted_coordinates = tuple([coordinate for coordinate in coordinates if approximate_x in coordinate[0]])
+
+    return sorted_coordinates
+
+
 # Read in the coordinates from the text file
 with open('s1 blade.txt', 'r') as f:
     raw_data = f.read().strip().splitlines()
@@ -15,51 +54,18 @@ for line in raw_data:
     coord_set = line.split(',')
 
     # Add each coordinate to its corresponding list and apply its scientific notation
-    x_value = str(float(coord_set[0][:-4]) / 10**(int(coord_set[0][-1:])))
-    y_value = str(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:])))
-    z_value = str(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:])))
-    
-    x_values.append(x_value)
-    y_values.append(x_value)
-    z_values.append(x_value)
-    
-# Coordinates as points
-unsorted_coordinates = tuple(zip(x_values, y_values, z_values))
+    x_values.append(str(float(coord_set[0][:-4]) / 10**(int(coord_set[0][-1:]))))
+    y_values.append(str(float(coord_set[1][:-4]) / 10**(int(coord_set[1][-1:]))))
+    z_values.append(str(float(coord_set[2][:-4]) / 10**(int(coord_set[2][-1:]))))
 
+sorted_x = x_values.copy()
+sorted_x.sort()
 
-tip = max(x_value)
-root = min(x_value)
-sorted_x = x_values.sort()
+x_location = 0.5 # Spanwise location of the blade
+precision = 4 # How many digits to include from each coordinate
 
-# Identify the coordinates around this general location 
-approximate_x = '0.598'
-
-# Filter the coordinates by the general location
-push_coordinates = [coordinate for coordinate in unsorted_coordinates if coordinate[0].find(approximate_x) != -1]
-
-'''
-Write the injection locations to a file
-
-NOTE: Code works. Don't change it.
-'''
+# Get the coordintes for a user-defined spanwise location
+sorted_coordinates = cross_section(x_location, precision)
 
 filename = 'test regions.csv'
-
-# Erase the contents of the file
-open(filename, 'w').close()
-
-# Open the file to use csv module
-with open(filename, 'w', newline='\n') as csvfile:
-    coord_writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-
-    # File Format Requirements
-    csvfile.write('[Name]\nS1 Blade\n\n')
-    csvfile.write('[Spatial Fields]\n')
-    coord_writer.writerow(['X', 'Y', 'Z'])
-    csvfile.write('\n[Data]\n')
-    coord_writer.writerow(['X[m]', 'Y[m]', 'Z[m]'])
-    csvfile.write('\n')
-
-    # Write each set of coordinates as a row in the file
-    for point in push_coordinates:
-        coord_writer.writerow([point[0], point[1], point[2]])
+write_coords(sorted_coordinates, filename)
