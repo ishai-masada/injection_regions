11353846ee62d062bf86230ff66ba601383463be
root <root@AEIM>
2024-11-01 22:36:49 -0400

    beh

--- curve_build/build_blade.py
+++ curve_build/build_blade.py
@@ -41,13 +41,11 @@ with open('data/half/span 0.5 rotor upper trailing.txt', 'r') as f:
     upper_trailing_05 = f.read().splitlines()
 
 # Span 0.75
-'''
 with open('data/3_4th/span 0.75 rotor lower.txt', 'r') as f:
     lower_075 = f.read().splitlines()
 
 with open('data/3_4th/span 0.75 rotor upper.txt', 'r') as f:
     upper_075 = f.read().splitlines()
-'''
 
 # Span 1.0
 with open('data/end/span 1.0 rotor lower.txt', 'r') as f:
@@ -60,8 +58,8 @@ with open('data/end/span 1.0 rotor upper trailing.txt', 'r') as f:
     upper_trailing_1 = f.read().splitlines()
 
 # Parameter
-resolution = 0.05
-t = numpy.arange(0, 1 + resolution, resolution)
+resolution = 0.01
+t = numpy.arange(0, 1, resolution)
 
 # Convert the data into the Position Vector type
 lower_0 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in lower_0]
@@ -79,6 +77,15 @@ upper_y_025 = [point.y_coord for point in upper_025]
 lower_05 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in lower_05]
 upper_05 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in upper_05]
 upper_trailing_05 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in upper_trailing_05]
+
+lower_075 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in lower_075]
+lower_x_075 = [point.x_coord for point in lower_075]
+lower_y_075 = [point.y_coord for point in lower_075]
+
+upper_075 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in upper_075]
+upper_x_075 = [point.x_coord for point in upper_075]
+upper_y_075 = [point.y_coord for point in upper_075]
+
 
 lower_1 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in lower_1]
 upper_1 = [Point(float(point.split('\t')[0]), float(point.split('\t')[1])) for point in upper_1]
@@ -106,6 +113,14 @@ upper_x_025 = numpy.linspace(min(upper_x_025), max(upper_x_025), 100)
 lower_curve_025 = interpolate.BSpline(*lower_tck_025)(lower_x_025)
 upper_curve_025 = interpolate.BSpline(*upper_tck_025)(upper_x_025)
 
+lower_tck_075 = interpolate.splrep(lower_x_075, lower_y_075, s=0, k=3)
+upper_tck_075 = interpolate.splrep(upper_x_075, upper_y_075, s=0, k=3)
+
+lower_x_075 = numpy.linspace(min(lower_x_075), max(lower_x_075), 100)
+upper_x_075 = numpy.linspace(min(upper_x_075), max(upper_x_075), 100)
+lower_curve_075 = interpolate.BSpline(*lower_tck_075)(lower_x_075)
+upper_curve_075 = interpolate.BSpline(*upper_tck_075)(upper_x_075)
+
 # Produce the curves
 lower_curve_0.bezier_function()
 upper_curve_0.bezier_function()
@@ -120,26 +135,23 @@ upper_curve_1.bezier_function()
 upper_trailing_1.bezier_function()
 
 # Plot the curves
-'''
 lower_curve_0.plot_points()
 upper_curve_0.plot_points()
 upper_trailing_0.plot_points()
 
 plt.plot(lower_x_025, lower_curve_025)
-plt.plot(upper_x_025, lower_curve_025)
+plt.plot(upper_x_025, upper_curve_025)
+
 
 lower_curve_05.plot_points()
 upper_curve_05.plot_points()
 upper_trailing_05.plot_points()
 
+plt.plot(lower_x_075, lower_curve_075)
+plt.plot(upper_x_075, upper_curve_075)
+
 lower_curve_1.plot_points()
 upper_curve_1.plot_points()
 upper_trailing_1.plot_points()
-
-'''
-
-plt.plot(lower_x_025, lower_curve_025)
-plt.plot(upper_x_025, upper_curve_025)
-
 
 plt.show()
---
+++ curve_build/heh.py
@@ -0,0 +1,3 @@
+chord_position = input('enter the desired chord position: ')
+span_position = input('enter the desired chord position: ')
+ps_ss = input('choose either pressure side or suction side (ps/ss): ')
